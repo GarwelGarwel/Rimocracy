9312861d1dcc2c944ebce93e45f3dd38176e68eb
GarwelGarwel <okozlovsky@gmail.com>
2020-06-10 12:44:19 +0300

    Added Rimocracy class

--- Source/Rimocracy.cs
+++ Source/Rimocracy.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
 
 namespace Rimocracy
 {
-    public class Class1
+    public class Rimocracy : MapComponent
     {
+        public Rimocracy(Map map) : base(map)
+        { }
     }
 }
