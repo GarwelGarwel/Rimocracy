070a73a745aeea3c9e7022ac25ef2da87bfff35b
GarwelGarwel <okozlovsky@gmail.com>
2020-06-11 08:46:48 +0300

    Added leader choice + logging

--- Source/Rimocracy.cs
+++ Source/Rimocracy.cs
@@ -1,15 +1,60 @@
-﻿using System;
+﻿using RimWorld;
+using RimWorld.Planet;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Verse;
 
 namespace Rimocracy
 {
-    public class Rimocracy : MapComponent
+    public class Rimocracy : WorldComponent
     {
-        public Rimocracy(Map map) : base(map)
+        Pawn leader;
+
+        bool initialized = false;
+
+        public Rimocracy()
+            : base(Find.World)
         { }
+
+        public Rimocracy(World world)
+            : base(world)
+        { }
+
+        public Pawn Leader
+        {
+            get => leader;
+            set => leader = value;
+        }
+
+        public override void ExposeData()
+        {
+            Scribe_References.Look(ref leader, "leader");
+        }
+
+        public override void WorldComponentTick()
+        {
+            if (initialized)
+                return;
+            Utility.Log("WorldComponentTick: initializing...");
+            if (leader == null)
+                ChooseLeader();
+            else Utility.Log("Current leader is " + leader);
+            initialized = true;
+        }
+
+        void ChooseLeader()
+        {
+            Utility.Log("ChooseLeader");
+            List<Pawn> candidates = PawnsFinder.AllMapsCaravansAndTravelingTransportPods_Alive_FreeColonists_NoCryptosleep;
+            leader = candidates.FirstOrDefault();
+            if (leader != null)
+                Utility.Log("New leader is " + leader);
+            else
+            {
+                Utility.Log("No suitable leader found. " + candidates.Count + " total candidate pawns:");
+                foreach (Pawn p in candidates)
+                    Utility.Log("- " + (p.IsColonist ? "COLONIST " : "") + p);
+            }
+        }
     }
 }
---
+++ Source/Utility.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace Rimocracy
+{
+    enum LogLevel { Message = 0, Warning, Error };
+
+    static class Utility
+    {
+        public static void Log(string message, LogLevel logLevel = LogLevel.Message)
+        {
+            message = "[Rimocracy] " + message;
+            switch (logLevel)
+            {
+                case LogLevel.Message:
+                    Verse.Log.Message(message);
+                    break;
+
+                case LogLevel.Warning:
+                    Verse.Log.Warning(message);
+                    break;
+
+                case LogLevel.Error:
+                    Verse.Log.Error(message);
+                    break;
+            }
+        }
+    }
+}
