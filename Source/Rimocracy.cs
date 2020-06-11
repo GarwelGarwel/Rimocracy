524f2783fd4467b1241306c0db1ae43a03397ee6
GarwelGarwel <okozlovsky@gmail.com>
2020-06-11 17:38:26 +0300

    Added Authority value and WorkSpeedGlobal stat effect

--- Source/Rimocracy.cs
+++ Source/Rimocracy.cs
@@ -9,7 +9,7 @@ namespace Rimocracy
     public class Rimocracy : WorldComponent
     {
         Pawn leader;
-
+        float authority = 0.1f;
         bool initialized = false;
 
         public Rimocracy()
@@ -20,15 +20,26 @@ namespace Rimocracy
             : base(world)
         { }
 
+        public static Rimocracy Instance => Find.World.GetComponent<Rimocracy>();
+
         public Pawn Leader
         {
             get => leader;
             set => leader = value;
         }
 
+        public float Authority
+        {
+            get => authority;
+            set => authority = value;
+        }
+
+        public static bool CanBeLeader(Pawn p) => p != null && !p.Dead && p.IsFreeColonist;
+
         public override void ExposeData()
         {
             Scribe_References.Look(ref leader, "leader");
+            Scribe_Values.Look(ref authority, "authority");
         }
 
         public override void WorldComponentTick()
@@ -36,7 +47,7 @@ namespace Rimocracy
             if (initialized)
                 return;
             Utility.Log("WorldComponentTick: initializing...");
-            if (leader == null)
+            if (leader == null || !CanBeLeader(leader))
                 ChooseLeader();
             else Utility.Log("Current leader is " + leader);
             initialized = true;
@@ -46,15 +57,14 @@ namespace Rimocracy
         {
             Utility.Log("ChooseLeader");
             List<Pawn> candidates = PawnsFinder.AllMapsCaravansAndTravelingTransportPods_Alive_FreeColonists_NoCryptosleep;
-            leader = candidates.FirstOrDefault();
+            leader = candidates.FirstOrDefault(p => CanBeLeader(p));
             if (leader != null)
-                Utility.Log("New leader is " + leader);
-            else
             {
-                Utility.Log("No suitable leader found. " + candidates.Count + " total candidate pawns:");
-                foreach (Pawn p in candidates)
-                    Utility.Log("- " + (p.IsColonist ? "COLONIST " : "") + p);
+                Utility.Log("New leader is " + leader + " (chosen from " + candidates.Count + " candidates).");
+                authority = (float)(leader.skills.GetSkill(SkillDefOf.Intellectual).Level + leader.skills.GetSkill(SkillDefOf.Social).Level) / 40;
+                Utility.Log("Authority level is " + authority.ToString("P0"));
             }
+            else Utility.Log("No suitable leader found. " + candidates.Count + " total candidate pawns:");
         }
     }
 }
---
+++ Source/StatPart_Authority.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Rimocracy
+{
+    public class StatPart_Authority : StatPart
+    {
+        float AuthorityMultiplier => 1 + Rimocracy.Instance.Authority * 0.1f;
+
+        public override string ExplanationPart(StatRequest req)
+        {
+            if (AppliesTo(req))
+                return "Aurhority " + Rimocracy.Instance.Authority.ToString("P0") + ": x" + AuthorityMultiplier.ToString("F2");
+            return "StatPart_Authority doesn't apply to " + (req.Thing?.ToString() ?? "nothing") + ".";
+        }
+
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            if (AppliesTo(req))
+                val *= AuthorityMultiplier;
+        }
+
+        bool AppliesTo(StatRequest req) => req.Thing is Pawn p && p.IsFreeColonist;
+    }
+}
