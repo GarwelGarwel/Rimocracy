f9769208d83de38eabd2ef530837f8fa54d11e63
GarwelGarwel <okozlovsky@gmail.com>
2020-06-12 23:47:10 +0300

    Added Lot and Seniority succession types

---
+++ Source/Succession/SuccessionLot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Rimocracy.Succession
+{
+    /// <summary>
+    /// Choose a random leader
+    /// </summary>
+    class SuccessionLot : SuccessionBase
+    {
+        public override string Title => "Lot";
+
+        public override Pawn ChooseLeader() => Candidates.RandomElementWithFallback();
+    }
+}
