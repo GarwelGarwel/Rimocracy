ccc8ac4683c936451da4e52d71e71d40d4c06a6b
GarwelGarwel <okozlovsky@gmail.com>
2020-06-14 15:22:19 +0300

    Added Respect thought

---
+++ Source/ThoughtWorker_Respect.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Rimocracy
+{
+    public class ThoughtWorker_Respect : ThoughtWorker
+    {
+        protected override ThoughtState CurrentSocialStateInternal(Pawn p, Pawn otherPawn)
+            => otherPawn == Rimocracy.Instance.Leader && p.ageTracker.AgeBiologicalYears >= 16;
+    }
+}
---
+++ Source/Thought_Respect.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+
+namespace Rimocracy
+{
+    class Thought_Respect : Thought_SituationalSocial
+    {
+        protected override ThoughtState CurrentStateInternal()
+        {
+            if (otherPawn == Rimocracy.Instance.Leader && pawn.ageTracker.AgeBiologicalYears >= 16)
+            {
+                if (Rimocracy.Instance.Authority < 0.10)
+                    return ThoughtState.ActiveAtStage(0);
+                else if (Rimocracy.Instance.Authority < 0.25)
+                    return ThoughtState.ActiveAtStage(1);
+                else if (Rimocracy.Instance.Authority < 0.75)
+                    return ThoughtState.ActiveAtStage(2);
+                else if (Rimocracy.Instance.Authority < 0.95)
+                    return ThoughtState.ActiveAtStage(3);
+                return ThoughtState.ActiveAtStage(4);
+            }
+            return false;
+        }
+    }
+}
