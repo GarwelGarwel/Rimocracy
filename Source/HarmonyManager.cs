130c6d418dc92784ed98330ee0e0f3f01276f01d
GarwelGarwel <okozlovsky@gmail.com>
2020-06-19 08:45:47 +0300

    Added Harmony as a dependency

---
+++ Source/HarmonyManager.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Rimocracy
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyManager
+    {
+        static Harmony harmony;
+
+        static HarmonyManager()
+        {
+            harmony = new Harmony("Garwel.Rimocracy");
+        }
+    }
+}
