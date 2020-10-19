37977cfbc6faca898f9c7a8055ed00d5e1529186
GarwelGarwel <okozlovsky@gmail.com>
2020-10-19 10:06:55 +0300

    Added dummy StatWorker for GovernanceDecay stat

---
+++ Source/Stats/StatWorker_GovernanceDecay.cs
@@ -0,0 +1,10 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Rimocracy
+{
+    public class StatWorker_GovernanceDecay : StatWorker
+    {
+        public override bool IsDisabledFor(Thing thing) => !(thing as Pawn).IsLeader();
+    }
+}
