8bb99aeedffd4246358d83b42f71e33fa2762c23
GarwelGarwel <okozlovsky@gmail.com>
2021-02-09 09:44:55 +0300

    Fixed ResPublica thought stages

--- Source/Thoughts/ThoughtWorker_ResPublica.cs
+++ Source/Thoughts/ThoughtWorker_ResPublica.cs
@@ -7,19 +7,19 @@ namespace Rimocracy
     {
         protected override ThoughtState CurrentStateInternal(Pawn p)
         {
-            if (!Utility.RimocracyComp.DecisionActive("ResPublica"))
+            if (!Utility.RimocracyComp.DecisionActive("ResPublica") || !p.IsCitizen())
                 return ThoughtState.Inactive;
 
             float g = Utility.RimocracyComp.Governance;
-            if (g < 0.10)
+            if (g < 0.05f)
                 return ThoughtState.ActiveAtStage(0);
-            else if (g < 0.25)
+            else if (g < 0.25f)
                 return ThoughtState.ActiveAtStage(1);
-            else if (g < 0.75)
+            else if (g < 0.75f)
                 return ThoughtState.Inactive;
-            else if (g < 0.95)
-                return ThoughtState.ActiveAtStage(3);
-            return ThoughtState.ActiveAtStage(4);
+            else if (g < 0.95f)
+                return ThoughtState.ActiveAtStage(2);
+            return ThoughtState.ActiveAtStage(3);
         }
     }
 }
