bfc20b7ada7d5d2ecd2c8a59a220902517b0e862
GarwelGarwel <okozlovsky@gmail.com>
2021-09-15 09:42:55 +0300

    CompCitizen is added to intelligent pawns; loyalty is updated based on mood

--- Source/ThingComps/CompCitizen.cs
+++ Source/ThingComps/CompCitizen.cs
@@ -1,20 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using UnityEngine;
 using Verse;
 
-namespace Rimocracy.ThingComps
+namespace Rimocracy
 {
     public class CompCitizen : ThingComp
     {
         float loyalty;
 
+        public CompCitizen()
+        { }
+
+        public CompCitizen(Pawn parent) => Pawn = parent;
+
         public float Loyalty
         { 
-            get => loyalty; 
-            set => loyalty = value; 
+            get => loyalty;
+            set => loyalty = Mathf.Clamp(value, -100, 100);
+        }
+
+        public Pawn Pawn
+        {
+            get => parent as Pawn;
+            set => parent = value;
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref loyalty, "loyalty");
+        }
+
+        public override string CompInspectStringExtra() => Pawn.IsCitizen() ? $"Loyalty: {Loyalty:N1}" : null;
+
+        public override void CompTickRare()
+        {
+            // Don't update Loyalty if politics is disabled, for non-citizens, unconscious and sleeping pawns
+            if (!Utility.PoliticsEnabled || !Pawn.IsCitizen() || !Pawn.health.capacities.CanBeAwake || (Pawn.jobs?.curDriver != null && Pawn.jobs.curDriver.asleep))
+                return;
+
+            if (Pawn.needs?.mood == null)
+            {
+                Utility.Log($"{Pawn}'s mood is null.", LogLevel.Error);
+                return;
+            }
+
+            loyalty += (Pawn.needs.mood.CurLevelPercentage - 0.5f) / 18;
         }
     }
 }
