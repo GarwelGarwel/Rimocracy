d9e423f93c1b0ebc092a0aeae215e3081ceabb8a
Garwel <garwel@protonmail.com>
2022-05-19 15:03:26 +0600

    Added TimeProtesting record

---
+++ Source/RecordWorker_TimeProtesting.cs
@@ -0,0 +1,10 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Rimocracy
+{
+    class RecordWorker_TimeProtesting : RecordWorker
+    {
+        public override bool ShouldMeasureTimeNow(Pawn pawn) => pawn.GetLoyalty().IsProtesting;
+    }
+}
