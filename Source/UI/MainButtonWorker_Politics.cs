d97440b59b40574779ec7562e157fd5b56a8460e
Garwel <garwel@protonmail.com>
2023-08-15 23:32:29 +0300

    Progress bar on main tab button

---
+++ Source/UI/MainButtonWorker_Politics.cs
@@ -0,0 +1,9 @@
+﻿using RimWorld;
+
+namespace Rimocracy
+{
+    public class MainButtonWorker_Politics : MainButtonWorker_ToggleTab
+    {
+        public override float ButtonBarPercent => Utility.PoliticsEnabled ? Utility.RimocracyComp.Governance : 0;
+    }
+}
