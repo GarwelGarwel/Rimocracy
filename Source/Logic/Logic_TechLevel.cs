428296bded239e7cf4fba4ad49d9f77ebf75c8dc
Garwel <garwel@protonmail.com>
2024-03-09 09:23:32 +0100

    Added higherOrEqual techLevel logic

--- Source/Logic/Logic_TechLevel.cs
+++ Source/Logic/Logic_TechLevel.cs
@@ -8,14 +8,26 @@ namespace Rimocracy
     public class Logic_TechLevel : Logic_Simple
     {
         TechLevel level = TechLevel.Undefined;
+        TechLevel higherOrEqual = TechLevel.Undefined;
         List<TechLevel> any = new List<TechLevel>();
 
-        string LevelCommaList => level != TechLevel.Undefined ? level.ToStringHuman() : any.Select(techLevel => techLevel.ToStringHuman()).ToCommaListOr();
+        string LevelCommaList =>
+            level != TechLevel.Undefined ? level.ToStringHuman()
+            : (higherOrEqual != TechLevel.Undefined ? $"at least {higherOrEqual.ToStringHuman()}"
+            : any.Select(techLevel => techLevel.ToStringHuman()).ToCommaListOr());
 
         public override string DefaultLabel => $"tech level is {LevelCommaList}";
 
         public override string LabelInverted => $"tech level is not {LevelCommaList}";
 
-        protected override bool IsSatisfiedInternal(Pawn pawn, Pawn target) => Faction.OfPlayer.def.techLevel == level || any.Contains(Faction.OfPlayer.def.techLevel);
+        protected override bool IsSatisfiedInternal(Pawn pawn, Pawn target)
+        {
+            if (Faction.OfPlayer?.def == null)
+                return false;
+            TechLevel techLevel = Faction.OfPlayer.def.techLevel;
+            return techLevel == level
+                || (higherOrEqual != TechLevel.Undefined && techLevel >= higherOrEqual)
+                || any.Contains(techLevel);
+        }
     }
 }
